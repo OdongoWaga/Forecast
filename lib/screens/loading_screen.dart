fb0525f04ae8487a465281f048fd39f5c4d66b54
Waga <wagaodongo@gmail.com>
2019-08-12 17:57:29 +0300

    spinner

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:forecast/services/location.dart';
+import 'package:forecast/services/networking.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = '19e3e751ac70f9a17b75c81ea15c0d5d';
 
@@ -16,10 +19,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
 
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
 
     await location.getCurrentLocation();
@@ -27,30 +30,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
     latitude = location.latitude;
     longitude = location.longitude;
 
-    getData();
-  }
-
-  void getData() async {
-    http.Response response = await http.get(
+    NetworkHelper networkHelper = NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
-    if (response.statusCode == 200) {
-      String data = response.body;
+    var weatherData = await networkHelper.getData();
 
-      double temperature = jsonDecode(data)['main']['temp'];
-      int condition = jsonDecode(data)['weather'][0]['id'];
-      String cityName = jsonDecode(data)['name'];
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -5,4 +5,18 @@ class NetworkHelper {
   NetworkHelper(this.url);
 
   final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      return decodedData;
+    } else {
+      print(response.statusCode);
+    }
+  }
 }
