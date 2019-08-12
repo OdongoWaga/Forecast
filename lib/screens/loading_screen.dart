3de5d6f77c96dc509fb734ada17b849b46964c2c
Waga <wagaodongo@gmail.com>
2019-08-12 14:26:42 +0300

    Current Location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,23 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
-  void getLocation() async {
-    Position position = await Geolocator()
-        .getLastKnownPosition(desiredAccuracy: LocationAccuracy.low);
-  }
-
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
